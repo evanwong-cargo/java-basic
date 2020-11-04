fdb4f3b2633c4ad629fbff772168cec276df7f91
EvanWong <evanwong@cargofe.com>
2020-11-04 14:22:56 +0800

    commit-02

--- src/Test01.java
+++ src/Test01.java
@@ -3,6 +3,7 @@ public class Test01 {
     public static void main(String[] args) {
         try {
             System.out.println("heheh 01");
+            System.out.println("2222");
         } catch (Exception e) {
             e.printStackTrace();
         }
